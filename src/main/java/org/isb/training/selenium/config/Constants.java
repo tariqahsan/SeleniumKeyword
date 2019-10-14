10ddbc5bc964fae512b08c6ba5bc3649ead41870
Tariq Ahsan <tariqahsan@yahoo.com>
2019-10-13 20:20:54 -0400

    Clean-up of Constants file

--- src/main/java/org/isb/training/selenium/config/Constants.java
+++ src/main/java/org/isb/training/selenium/config/Constants.java
@@ -8,24 +8,20 @@ public class Constants {
 	
 	//System Variables
 	public static final String URL = "http://www.store.demoqa.com";
-	//public static final String Path_TestData = "D://Tools QA Projects//trunk//Hybrid KeyWord Driven//src//dataEngine//DataEngine.xlsx";
 	private static String FILE_EXTENSION = ".xlsx";
 	public static final String FILE_PATH = "TestResults//TestResult_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMMdd_HH-mm-ss")) + FILE_EXTENSION ;
-	//public static final String Path_OR = "D://Tools QA Projects//trunk//Hybrid KeyWord Driven//src//config//OR.txt";
-	public static final String Path_OR = "config//OR.txt";
-	public static final String File_TestData = "DataEngine.xlsx";
 	public static final String KEYWORD_FAIL = "FAIL";
 	public static final String KEYWORD_PASS = "PASS";
 	
 	//Data Sheet Column Numbers
 	public static final int Col_TestCaseID = 0;	
-	public static final int Col_TestScenarioID =1 ;
-	public static final int Col_PageObject =4 ;
-	public static final int Col_ActionKeyword =5 ;
-	public static final int Col_RunMode =2 ;
-	public static final int Col_Result =3 ;
-	public static final int Col_DataSet =6 ;
-	public static final int Col_TestStepResult =7 ;
+	public static final int Col_TestScenarioID = 1 ;
+	public static final int Col_PageObject = 4 ;
+	public static final int Col_ActionKeyword = 5 ;
+	public static final int Col_RunMode = 2 ;
+	public static final int Col_Result = 3 ;
+	public static final int Col_DataSet = 6 ;
+	public static final int Col_TestStepResult = 7 ;
 		
 	// Data Engine Excel sheets
 	public static final String Sheet_TestSteps = "Test Steps";
