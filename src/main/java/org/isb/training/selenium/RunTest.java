fbce604806559c1fb3b6e1f2f276987bd3b1cb01
Tariq Ahsan <tariqahsan@yahoo.com>
2019-10-13 20:10:52 -0400

    Removed unused and modified codes

--- src/main/java/org/isb/training/selenium/RunTest.java
+++ src/main/java/org/isb/training/selenium/RunTest.java
@@ -1,42 +1,54 @@
 package org.isb.training.selenium;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import org.apache.log4j.Logger;
-
-import org.openqa.selenium.By;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import jxl.Sheet;
-import jxl.read.biff.BiffException;
-
 import org.isb.training.selenium.ConfigFileReader;
 import org.isb.training.selenium.Driver;
+import org.isb.training.selenium.config.Constants;
 import org.isb.training.selenium.CommonFunctionsLib;
 
 
 public class RunTest {
-	
+
 	final static Logger logger = Logger.getLogger(RunTest.class);
+	public static boolean result;
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	Map<String, Object[]> data;
+
 	private WebDriver webDriver;
-    Driver driver;
+	Driver driver;
 
 	@BeforeClass
 	public void beforeClass() {
-		// copy from Test Automation src>driver.java> paste here chrome driver
-		// executable file
+
 		System.setProperty("webdriver.chrome.driver", "./webdriver/chromedriver.exe");
-		// driver=new FirefoxDriver();
-//		driver = new ChromeDriver();
 		driver = new Driver();
+
+		// Blank workbook 
+		workbook = new XSSFWorkbook(); 
+
+		// Create a blank sheet 
+		sheet = workbook.createSheet("Chrome Test"); 
+
+		// This data needs to be written (Object[]) 
+		data = new TreeMap<String, Object[]>(); 
+		// Header
+		data.put("1", new Object[]{ "Test Step Case Number", "Test Step Description", "Element", "Value", "Keyword", "Test Result"}); 
 	}
 
 	@AfterClass
@@ -45,30 +57,24 @@ public class RunTest {
 	}
 
 	@Test
-	public void verifySearchButton() throws BiffException, IOException, NumberFormatException, InterruptedException {
-
+	public void verifySearchButton() throws Exception {
 
 		// creating class object for configFileReader 
 		ConfigFileReader configFileReader = new ConfigFileReader();
 		ExcelSheetDriver excelSheetDriver = new ExcelSheetDriver();
 		ExcelSheetDriver excelSheetDriver1 = new ExcelSheetDriver();
 		ExcelSheetDriver excelSheetDriver2 = new ExcelSheetDriver();
-		
+
 		// variable name testSuitesheet data type is Sheet
 		Sheet testSuitesheet = excelSheetDriver.getWorksheet(configFileReader.getTestSuite(), configFileReader.getTestSuiteSheet());
-		int c = excelSheetDriver.columnCount();  // 7
-		int r = excelSheetDriver.rowCount();  // 44
-		System.out.println(c + " " + r);
+		int c = excelSheetDriver.columnCount();
+		int r = excelSheetDriver.rowCount();
 
 		for (int i = 1; i < r; i++) {
-//			String SNo = excelSheetDriver.readCell(testSuitesheet,0, i);
-			String SNo = excelSheetDriver.readCell(testSuitesheet, 0, i);
 
+			String SNo = excelSheetDriver.readCell(testSuitesheet, 0, i);
 			String Description = excelSheetDriver.readCell(testSuitesheet, 1, i);
 			String ExecutionFlag = excelSheetDriver.readCell(testSuitesheet, 2, i);
-//			logger.info("TestSuite:" + SNo);
-//			logger.info("TestSuite:" + Description);
-//			logger.info("TestSuite:" + ExecutionFlag);
 			System.out.println("TestSuite:" + SNo);
 			System.out.println("TestSuite:" + Description);
 			System.out.println("TestSuite:" + ExecutionFlag);
@@ -112,11 +118,17 @@ public class RunTest {
 								logger.info("element:" + element);
 								logger.info("value:" + value);
 								logger.info("keywordTestSteps:" + keywordTestSteps);
-								
-//								logger.info("Executing performActions Method with the three arguments -" + keywordTestSteps + " " + value + " "+  element);
+
+								logger.info("Executing performActions Method with the three arguments -" + keywordTestSteps + " " + value + " "+  element);
 								System.out.println("Executing performActions Method with the three arguments -" + "keyword :" + keywordTestSteps + " " + "Value: " + value + " "+ "element: " + element);
-								boolean result = comlib.performActions(keywordTestSteps, value, element);
-								System.out.println("RESULT -> " + result);
+								result = comlib.performActions(keywordTestSteps, value, element);
+								if(result == true){
+									data.put(Integer.toString(w), new Object[]{ snoTestSteps, desTestSteps, element, value, keywordTestSteps, Constants.KEYWORD_PASS }); 
+									writeToExcel(data);
+								} else {
+									data.put(Integer.toString(w), new Object[]{ snoTestSteps, desTestSteps, element, value, keywordTestSteps, Constants.KEYWORD_FAIL }); 
+									writeToExcel(data);
+								}
 							}
 
 						}
@@ -132,5 +144,35 @@ public class RunTest {
 		excelSheetDriver1.closeworkbook();
 
 	}
+
+	private void writeToExcel(Map<String, Object[]> data) {
+		// Iterate over data and write to sheet 
+		Set<String> keyset = data.keySet(); 
+		int rownum = 0; 
+		for (String key : keyset) { 
+			// this creates a new row in the sheet 
+			Row row = sheet.createRow(rownum++); 
+			Object[] objArr = data.get(key); 
+			int cellnum = 0; 
+			for (Object obj : objArr) { 
+				// this line creates a cell in the next column of that row 
+				Cell cell = row.createCell(cellnum++); 
+				if (obj instanceof String) 
+					cell.setCellValue((String)obj); 
+				else if (obj instanceof Integer) 
+					cell.setCellValue((Integer)obj); 
+			} 
+		} 
+		try { 
+
+			// this Writes the workbook test result			
+			FileOutputStream out = new FileOutputStream(new File(Constants.FILE_PATH)); 
+			workbook.write(out); 
+			out.close(); 
+		} 
+		catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+	} 
 
 }
